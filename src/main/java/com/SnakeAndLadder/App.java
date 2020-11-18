156ccde669e106187bfc73047d09b2ea689dd589
saurav004 <logtosaurav@gmail.com>
2020-11-18 23:34:10 +0530

    Added:Player rolls the dice until they win

--- src/main/java/com/SnakeAndLadder/App.java
+++ src/main/java/com/SnakeAndLadder/App.java
@@ -8,20 +8,38 @@ import java.util.Random;
  */
 public class App {
 	static int currentPosition = 0;
-	static int SNAKE=2;
-	static int LADDER=1;
-	static int NOPLAY=0;
+	static int SNAKE = 2;
+	static int LADDER = 1;
+	static int NOPLAY = 0;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Program");
 
-	int move=checkIfMovingAheadOrBack();
-	
-	if(move>NOPLAY) {
-		if(move==LADDER) {
-			currentPosition+=rollDice();
+		while (true) {
+			int move = checkIfMovingAheadOrBack();
+			if (move > NOPLAY) {
+				if (move == LADDER) {
+					currentPosition += rollDice();
+				} else
+					currentPosition -= rollDice();
+			}
+			if (checkIfWon()) {
+				break;
+			}
 		}
-		else currentPosition-=rollDice();
 	}
+
+	private static boolean checkIfWon() {
+		System.out.println(currentPosition);
+		// TODO Auto-generated method stub
+		if (currentPosition == 100)
+			return true;
+		else {
+			if (currentPosition < 0) {
+				currentPosition = 0;
+			}
+			return false;
+		}
 	}
 
 	private static int checkIfMovingAheadOrBack() {

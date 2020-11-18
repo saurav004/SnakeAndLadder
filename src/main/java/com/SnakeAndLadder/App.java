1b26564a2536285813bf05b42301f2948d801a29
saurav004 <logtosaurav@gmail.com>
2020-11-19 01:23:49 +0530

    Added:Two Players Can play against eachother

--- src/main/java/com/SnakeAndLadder/App.java
+++ src/main/java/com/SnakeAndLadder/App.java
@@ -2,41 +2,63 @@ package com.SnakeAndLadder;
 
 import java.util.Random;
 
-/**
- * Hello world!
- *
- */
 public class App {
-	static int currentPosition = 0;
+	static int player1 = 0, player2 = 0;;
 	static int SNAKE = 2;
 	static int LADDER = 1;
 	static int NOPLAY = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Program");
+		int currentPlayer = -1;
 		int rollCountToWin = 0;
 		while (true) {
 			int move = checkIfMovingAheadOrBack();
-			if (move > NOPLAY) {
-				int diceValue = rollDice();
-				rollCountToWin++;
-				if (move == LADDER) {
-					currentPosition += diceValue;
-					if (currentPosition > 100)
-						currentPosition -= diceValue;
-				} else {
-					currentPosition -= diceValue;
+			if (currentPlayer == -1) {
+				if (move > NOPLAY) {
+					int diceValue = rollDice();
+					rollCountToWin++;
+					if (move == LADDER) {
+						currentPlayer = -currentPlayer;
+						player1 += diceValue;
+						if (player1 > 100)
+							player1 -= diceValue;
+					} else {
+						player1 -= diceValue;
+					}
+				}
+				if (checkIfWon(player1, currentPlayer)) {
+					break;
+				}
+			} else {
+				if (move > NOPLAY) {
+					int diceValue = rollDice();
+					rollCountToWin++;
+					if (move == LADDER) {
+						currentPlayer = -currentPlayer;
+						player2 += diceValue;
+						if (player2 > 100)
+							player2 -= diceValue;
+					} else {
+						player2 -= diceValue;
+					}
+				}
+				if (checkIfWon(player2, currentPlayer)) {
+					break;
 				}
 			}
-			if (checkIfWon()) {
-				break;
-			}
+			currentPlayer = -currentPlayer;
+
 		}
 		System.out.println("Number of times Dice was rolled was:" + rollCountToWin);
 	}
 
-	private static boolean checkIfWon() {
-		System.out.println("Current position" + currentPosition);
+	private static boolean checkIfWon(int player, int currentPlayer) {
+		int currentPosition = player;
+		if (currentPlayer == -1)
+			System.out.println("player 1 current position" + currentPosition);
+		else
+			System.out.println("player 2 current position" + currentPosition);
 		if (currentPosition == 100)
 			return true;
 		else {
@@ -48,7 +70,6 @@ public class App {
 	}
 
 	private static int checkIfMovingAheadOrBack() {
-		// TODO Auto-generated method stub
 		Random randomGenerator = new Random();
 		int randomValue = randomGenerator.nextInt(3);
 		return randomValue;
@@ -58,6 +79,7 @@ public class App {
 
 		Random randomGenerator = new Random();
 		int diceValue = randomGenerator.nextInt(6) + 1;
+		System.out.println(diceValue + " Dice");
 		return diceValue;
 
 	}

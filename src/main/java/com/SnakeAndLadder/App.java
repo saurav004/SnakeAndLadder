84efb0b9f90da93b21547e281ca6149b171a0037
saurav004 <logtosaurav@gmail.com>
2020-11-18 22:32:15 +0530

    Added:Die gets rolled once

--- src/main/java/com/SnakeAndLadder/App.java
+++ src/main/java/com/SnakeAndLadder/App.java
@@ -1,5 +1,7 @@
 package com.SnakeAndLadder;
 
+import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,17 @@ public class App {
 	static int currentPosition = 0;
 
 	public static void main(String[] args) {
+		rollDice();
 		System.out.println("Welcome to Snake and Ladder Program");
+
+	}
+
+	static int rollDice() {
+
+		Random randomGenerator = new Random();
+		int diceValue = randomGenerator.nextInt(6) + 1;
+		System.out.println(diceValue);
+		return diceValue;
+
 	}
 }

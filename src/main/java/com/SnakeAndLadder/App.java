75edf112f478ad83219fa4b24d5f34c44f8e1045
saurav004 <logtosaurav@gmail.com>
2020-11-18 22:12:54 +0530

    Added:'Welcome to Snake and Ladder Program' message added

---
+++ src/main/java/com/SnakeAndLadder/App.java
@@ -0,0 +1,11 @@
+package com.SnakeAndLadder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Program");
+	}
+}

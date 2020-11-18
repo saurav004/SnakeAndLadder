725519f2a906904185f01a5b343991f092fd5ba2
saurav004 <logtosaurav@gmail.com>
2020-11-18 23:16:06 +0530

    Added:check of if NoPlay, Ladder or Snake

--- src/main/java/com/SnakeAndLadder/App.java
+++ src/main/java/com/SnakeAndLadder/App.java
@@ -8,11 +8,27 @@ import java.util.Random;
  */
 public class App {
 	static int currentPosition = 0;
-
+	static int SNAKE=2;
+	static int LADDER=1;
+	static int NOPLAY=0;
 	public static void main(String[] args) {
-		rollDice();
 		System.out.println("Welcome to Snake and Ladder Program");
 
+	int move=checkIfMovingAheadOrBack();
+	
+	if(move>NOPLAY) {
+		if(move==LADDER) {
+			currentPosition+=rollDice();
+		}
+		else currentPosition-=rollDice();
+	}
+	}
+
+	private static int checkIfMovingAheadOrBack() {
+		// TODO Auto-generated method stub
+		Random randomGenerator = new Random();
+		int randomValue = randomGenerator.nextInt(3);
+		return randomValue;
 	}
 
 	static int rollDice() {
